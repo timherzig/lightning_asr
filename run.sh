f344bb10137b9772158a3da990dec38a1e4c967b
Tim Herzig <tim.herzig@hotmail.com>
2022-11-07 11:44:46 +0100

    run script memory change

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ srun \
 --job-name==emformer_train_test \
 --nodes==1 \
 --gpus=1 \
---mem=42G \
+--mem=24G \
 --time=0-01:00 \
 --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_22.10-py3.sqsh \
 --container-mounts=/ds:/ds,/netscratch:/netscratch,"`pwd`":"`pwd`" \
